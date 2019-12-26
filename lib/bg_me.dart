49c173bc6fa80d18fb53ce2ba301e33985f22550
ftadev <akhlaghi.fatemeh@gmail.com>
2019-12-26 18:46:51 +0330

    + FadeTransition to my photo

--- lib/bg_me.dart
+++ lib/bg_me.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
-class MeBG extends StatelessWidget {
+class MeBG extends StatefulWidget {
+  @override
+  _MeBGState createState() => _MeBGState();
+}
+
+class _MeBGState extends State<MeBG> with TickerProviderStateMixin {
+  AnimationController animController;
+
+  @override
+  void initState() {
+    animController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2)
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    animController.forward();
+
     return Stack(
       children: <Widget>[
         Container(
@@ -14,10 +32,16 @@ class MeBG extends StatelessWidget {
           padding: EdgeInsets.only(top: 30, right: 20),
           child: Align(
             alignment: Alignment.bottomRight,
-            child: Image.asset(
-              'assets/me.png',
-              height: MediaQuery.of(context).size.height,
-              fit: BoxFit.fill,
+            child: FadeTransition(
+              opacity: Tween(
+                begin: 0.0,
+                end: 1.0
+              ).animate(animController),
+              child: Image.asset(
+                'assets/me.png',
+                height: MediaQuery.of(context).size.height,
+                fit: BoxFit.fill,
+              ),
             ),
           ),
         ),
