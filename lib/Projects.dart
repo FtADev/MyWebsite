41fa42516a73a22d33bb23cc49c56fca8460bab2
ftadev <akhlaghi.fatemeh@gmail.com>
2019-12-27 20:34:57 +0330

    + Testing Projects

--- lib/Projects.dart
+++ lib/Projects.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
 
 import 'dynamic_card.dart';
+import 'fade_in_ui.dart';
+import 'other_components.dart';
 
 class Projects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DynamicCard(child: Container());
+    return DynamicCard(
+      child: Container(
+        padding: EdgeInsets.all(50),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FadeIn(
+              2,
+              Text(
+                "My Projects",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      FadeIn(2.5, CardPlaceholder()),
+                      FadeIn(3, CardPlaceholder()),
+                      FadeIn(3.5, CardPlaceholder()),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      FadeIn(2.5, CardPlaceholder()),
+                      FadeIn(3, CardPlaceholder()),
+                      FadeIn(3.5, CardPlaceholder()),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      FadeIn(2.5, CardPlaceholder()),
+                      FadeIn(3, CardPlaceholder()),
+                      FadeIn(3.5, CardPlaceholder()),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
