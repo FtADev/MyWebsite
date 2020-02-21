b67aacf80649d36259d1e080cfb447b5ddcc9eac
ftadev <akhlaghi.fatemeh@gmail.com>
2020-02-21 11:39:20 +0330

    + back btn

--- lib/ui/page2.dart
+++ lib/ui/page2.dart
@@ -25,29 +25,42 @@ class _Page2State extends State<Page2> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: AlignmentDirectional.topCenter,
-        children: <Widget>[
-          Directionality(
-            child: PageIndicatorContainer(
-              child: PageView.builder(
-                  onPageChanged: (pos) {},
-                  itemCount: pages.length,
-                  controller: _pageController,
-                  itemBuilder: (BuildContext context, index) {
-                    return Page(pages[index]);
-                  }),
-              align: IndicatorAlign.bottom,
-              length: pages.length,
-              indicatorColor: Colors.grey[300],
-              indicatorSelectorColor: Colors.grey,
-              padding: const EdgeInsets.only(bottom: 36.0),
-              shape: IndicatorShape.circle(size: 15.0),
+      body: Stack(children: [
+        Align(
+            alignment: Alignment.topLeft,
+            child: GestureDetector(
+              child: Container(
+                margin: EdgeInsets.only(left: 10, top: 20),
+                child: Icon(Icons.chevron_left, color: Colors.grey[600], size: 30,),
+              ),
+              onTap: () => Navigator.of(context).pop(),
+            )
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 35),
+            child: Directionality(
+              child: PageIndicatorContainer(
+                child: PageView.builder(
+                    onPageChanged: (pos) {},
+                    itemCount: pages.length,
+                    controller: _pageController,
+                    itemBuilder: (BuildContext context, index) {
+                      return Page(pages[index]);
+                    }),
+                align: IndicatorAlign.bottom,
+                length: pages.length,
+                indicatorColor: Colors.grey[300],
+                indicatorSelectorColor: Colors.grey,
+                padding: const EdgeInsets.only(bottom: 36.0),
+                shape: IndicatorShape.circle(size: 15.0),
+              ),
+              textDirection: TextDirection.ltr,
             ),
-            textDirection: TextDirection.ltr,
           ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 }
