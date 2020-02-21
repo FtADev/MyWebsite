18d8a167e925055f0be069fa428cee3fdc3be612
ftadev <akhlaghi.fatemeh@gmail.com>
2020-02-21 13:44:04 +0330

    Change top of about

--- lib/ui/page.dart
+++ lib/ui/page.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
-
-import 'my_custom_shape.dart';
+import 'package:flutter/painting.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'about_icons.dart';
+import 'my_custom_shape.dart';
 import 'screen.dart';
-
 
 final pages = [
   PageViewModel(
-    'Starting',
-    'My programming world was created on 2015 since I went to college! I said \'Hello, World\' with C++, and learnt basics of programming with it. After that I started to learn Java. I practice more and learnt Java Core. And then I tested a bit C# as a Windows Form, too :)',
-    false
-  ),
+      'Starting',
+      'My programming world was created on 2015 since I went to college! I said \'Hello, World\' with C++, and learnt basics of programming with it. After that I started to learn Java. I practice more and learnt Java Core. And then I tested a bit C# as a Windows Form, too :)',
+      false),
   PageViewModel(
-    'Middling',
-    'After one year, rolling stone, I found my way arround Android Developing! I started android developing with java and after some experiences I switched to Kotlin, my lovely language!',
-    false
-  ),
+      'Middling',
+      'After one year, rolling stone, I found my way arround Android Developing! I started android developing with java and after some experiences I switched to Kotlin, my lovely language!',
+      false),
   PageViewModel(
-    'And Now...',
-    'On 2017, with 4 of my friends, made a small team and start working together with a lot of Motivation! After a year, we make the team bigger. And now I\'m in ExceptionalDev Team! The best team that I ever had...',
-    true
-  ),
+      'And Now...',
+      'On 2017, with 4 of my friends, made a small team and start working together with a lot of Motivation! After a year, we make the team bigger. And now I\'m in ExceptionalDev Team! The best team that I ever had...',
+      true),
 ];
 
 class PageViewModel {
@@ -31,7 +28,8 @@ class PageViewModel {
 
   PageViewModel(
     this.title,
-    this.text, this.isLastPage,
+    this.text,
+    this.isLastPage,
   );
 }
 
@@ -40,24 +38,22 @@ class Page extends StatelessWidget {
   final Screen screen;
 
   Page(
-    this.viewModel, this.screen,
+    this.viewModel,
+    this.screen,
   );
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(
-            width: MediaQuery.of(context).size.width * screen.wSize,
-            height: MediaQuery.of(context).size.height * screen.hSize,
-            child: MyCustomShape()
-        ),
+        AboutTop(screen: screen,),
         Container(
           margin: EdgeInsets.symmetric(horizontal: screen.hPaddingAbout),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(viewModel.title,
+              Text(
+                viewModel.title,
                 style: TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.bold,
@@ -68,7 +64,8 @@ class Page extends StatelessWidget {
               SizedBox(
                 height: screen.sizedBox15,
               ),
-              Text(viewModel.text,
+              Text(
+                viewModel.text,
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: screen.textAbout,
@@ -79,86 +76,87 @@ class Page extends StatelessWidget {
           ),
         ),
         SizedBox(height: screen.sizedBox),
-        viewModel.isLastPage ?
-        Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-                Row(
+        viewModel.isLastPage
+            ? Container(
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/gmail.png',
-                      width: screen.icon2About,
-                      height: screen.icon2About,
-                      fit: BoxFit.fitWidth,
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/gmail.png',
+                          width: screen.icon2About,
+                          height: screen.icon2About,
+                          fit: BoxFit.fitWidth,
+                        ),
+                        SizedBox(width: screen.sizedBox10),
+                        Text(
+                          'akhlaghi.fatemeh@gmail.com',
+                          style: TextStyle(
+                              fontFamily: 'dekko',
+                              fontSize: screen.textAbout,
+                              color: Colors.black),
+                        )
+                      ],
                     ),
-                    SizedBox(width: screen.sizedBox10),
-                    Text(
-                      'akhlaghi.fatemeh@gmail.com',
-                      style: TextStyle(
-                          fontFamily: 'dekko',
-                          fontSize: screen.textAbout,
-                          color: Colors.black
-                      ),
-                    )
+                    SizedBox(
+                      height: screen.sizedBox15,
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        InkWell(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50.0)),
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.white,
+                                )),
+                            child: Image.asset(
+                              'assets/github.png',
+                              width: screen.iconAbout,
+                              height: screen.iconAbout,
+                            ),
+                          ),
+                          onTap: githubLauncher,
+                        ),
+                        SizedBox(
+                          width: screen.sizedBox,
+                        ),
+                        InkWell(
+                          child: Image.asset(
+                            'assets/gitlab.png',
+                            width: screen.iconAbout,
+                            height: screen.iconAbout,
+                          ),
+                          onTap: gitlabLauncher,
+                        ),
+                        SizedBox(
+                          width: screen.sizedBox,
+                        ),
+                        InkWell(
+                          child: Image.asset(
+                            'assets/telegram.png',
+                            width: screen.iconAbout,
+                            height: screen.iconAbout,
+                          ),
+                          onTap: telegramLauncher,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-              SizedBox(
-                height: screen.sizedBox15,
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                    InkWell(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                            border: Border.all(width: 1, color: Colors.white,)
-                        ),
-                        child: Image.asset(
-                          'assets/github.png',
-                          width: screen.iconAbout,
-                          height: screen.iconAbout,
-                        ),
-                      ),
-                      onTap: githubLauncher,
-                  ),
-                  SizedBox(
-                    width: screen.sizedBox,
-                  ),
-                    InkWell(
-                      child: Image.asset(
-                        'assets/gitlab.png',
-                        width: screen.iconAbout,
-                        height: screen.iconAbout,
-                      ),
-                      onTap: gitlabLauncher,
-                    ),
-                  SizedBox(
-                    width: screen.sizedBox,
-                  ),
-                    InkWell(
-                      child: Image.asset(
-                        'assets/telegram.png',
-                        width: screen.iconAbout,
-                        height: screen.iconAbout,
-                      ),
-                      onTap: telegramLauncher,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        )
+              )
             : Container(),
       ],
     );
   }
-
 
   githubLauncher() async {
     const url = 'https://github.com/FtADev/';
@@ -186,5 +184,4 @@ class Page extends StatelessWidget {
       throw 'Could not launch $url';
     }
   }
-
 }
