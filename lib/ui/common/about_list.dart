b677e35d32c61a74536f1f0801025d304d723175
ftadev <akhlaghi.fatemeh@gmail.com>
2020-05-26 12:53:08 +0430

    Change about text

--- lib/ui/common/about_list.dart
+++ lib/ui/common/about_list.dart
@@ -3,14 +3,14 @@ import 'about_page.dart';
 final pages = [
   PageViewModel(
       'Starting',
-      'My programming world was created on 2015 since I went to college! I said \'Hello, World\' with C++, and learnt basics of programming with it. After that I started to learn Java. I practice more and learnt Java Core. And then I tested a bit C# as a Windows Form, too :)',
+      'My programming world was created on 2015 since I went to college! I said \'Hello, World\' with C++, and learnt basics of programming with it. After that I started to learn Java. I practiced more and learnt Java Core. And then I tested a bit C# as a Windows Form, too :)',
       false),
   PageViewModel(
       'Middling',
-      'After one year, rolling stone, I found my way arround Android Developing! I started android developing with java and after some experiences I switched to Kotlin, my lovely language!',
+      'After one year, I found my way arround Android Developing! I started android development with java and that I switched to Kotlin, my lovely language!',
       false),
   PageViewModel(
       'And Now...',
-      'On 2017, with 4 of my friends, made a small team and start working together with a lot of Motivation! After a year, we make the team bigger. And now I\'m in ExceptionalDev Team! The best team that I ever had...',
+      'On 2017, with 4 of my friends, made a small team and started working together with a lot of Motivation! After a year, we make the team bigger. And until these days I were in ExceptionalDev Team! But now I\'m looking for a new place to work!',
       true),
 ];
