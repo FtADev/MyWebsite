99fa1763ec9da32c5eabee365c1a8dd9f5ba283b
ftadev <akhlaghi.fatemeh@gmail.com>
2020-07-29 13:27:14 +0430

    Change about

--- lib/ui/common/about_list.dart
+++ lib/ui/common/about_list.dart
@@ -7,10 +7,10 @@ final pages = [
       false),
   PageViewModel(
       'Middling',
-      'After one year, I found my way arround Android Developing! I started android development with java and that I switched to Kotlin, my lovely language!',
+      'After one year, I found my way around Android Developing! I started android development with java and that I switched to Kotlin, my lovely language!',
       false),
   PageViewModel(
       'And Now...',
-      'On 2017, with 4 of my friends, made a small team and started working together with a lot of Motivation! After a year, we make the team bigger. And now I\'m in ExceptionalDev Team!',
+      'On 2017, with 4 of my friends, made a small team that named ExceptionalDev Team and started working together with a lot of Motivation! After a year, we make the team bigger. I was there until 2020 and now I\'m looking for a new job!',
       true),
 ];
