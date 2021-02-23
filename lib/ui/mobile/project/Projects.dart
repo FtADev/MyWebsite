c3eaf10768b7f00be0f8809b260e3f1f5cb1f303
ftadev <akhlaghi.fatemeh@gmail.com>
2021-02-23 19:51:37 +0330

    Add provider for projects

--- lib/ui/mobile/project/Projects.dart
+++ lib/ui/mobile/project/Projects.dart
@@ -1,48 +1,112 @@
-import 'package:flutter/material.dart';
 import 'package:MyWebsite/ui/common/project_list.dart';
-import 'package:MyWebsite/ui/component/fade_in_ui.dart';
 import 'package:MyWebsite/ui/component/project_item_mob.dart';
+import 'package:MyWebsite/ui/provider/projects_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skeleton_loader/skeleton_loader.dart';
 
-class Projects extends StatelessWidget {
+class Projects extends StatefulWidget {
   final screen;
 
-  const Projects({Key key, @required this.screen}) : super(key: key);
+  Projects({Key key, @required this.screen}) : super(key: key);
+
+  @override
+  _ProjectsState createState() => _ProjectsState();
+}
+
+class _ProjectsState extends State<Projects> {
+  ProjectViewModel viewModel = ProjectViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel.delay();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Projects", style: TextStyle(fontFamily: 'dekko',
+        title: Text(
+          "My Projects",
+          style: TextStyle(
+            fontFamily: 'dekko',
             fontSize: 18,
             fontWeight: FontWeight.bold,
-            color: Colors.black),),
+            color: Colors.black,
+          ),
+        ),
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: GestureDetector(
           child: Container(
-            child: Icon(Icons.chevron_left, color: Colors.grey[600],),
+            child: Icon(
+              Icons.chevron_left,
+              color: Colors.grey[600],
+            ),
           ),
           onTap: () => Navigator.of(context).pop(),
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 5),
-        child: ListView.builder(
-          itemCount: projectList.length,
-          itemBuilder: (context, index) {
-            return FadeIn(
-              index + 0.5,
-              ProjectItem(
-                title: projectList[index].title,
-                detail: projectList[index].detail,
-                image: projectList[index].image,
-                lang: projectList[index].lang,
-                colorLang: projectList[index].colorLang,
-                isTeamWork: projectList[index].isTeamWork,
-                url: projectList[index].url,
-              ),
-            );
-          },
+      body: ChangeNotifierProvider<ProjectViewModel>(
+        create: (context) => viewModel,
+        child: Consumer<ProjectViewModel>(
+          builder:
+              (BuildContext context, ProjectViewModel model, Widget child) =>
+                  Container(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: model.loading
+                ? SingleChildScrollView(
+                    child: SkeletonLoader(
+                      builder: Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        child: Row(
+                          children: <Widget>[
+                            CircleAvatar(
+                              backgroundColor: Colors.white,
+                              radius: 30,
+                            ),
+                            SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    width: double.infinity,
+                                    height: 10,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(height: 10),
+                                  Container(
+                                    width: double.infinity,
+                                    height: 12,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      items: projectList.length,
+                      period: Duration(seconds: 2),
+                      highlightColor: Colors.lightBlue[300],
+                      direction: SkeletonDirection.ltr,
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: projectList.length,
+                    itemBuilder: (context, index) => ProjectItem(
+                      title: projectList[index].title,
+                      detail: projectList[index].detail,
+                      image: projectList[index].image,
+                      lang: projectList[index].lang,
+                      colorLang: projectList[index].colorLang,
+                      isTeamWork: projectList[index].isTeamWork,
+                      url: projectList[index].url,
+                    ),
+                  ),
+          ),
         ),
       ),
     );
---
+++ lib/ui/provider/projects_view_model.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProjectViewModel extends ChangeNotifier {
+  bool _loading;
+
+  bool get loading => _loading;
+
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  Future delay() async {
+    loading = true;
+    await Future.delayed(Duration(seconds: 2));
+    loading = false;
+  }
+}
