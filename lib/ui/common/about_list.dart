56f7f798910246f37265cbb532fcc1a02e6eeae4
ftadev <akhlaghi.fatemeh@gmail.com>
2021-02-28 17:48:38 +0330

    Change about description

--- lib/ui/common/about_list.dart
+++ lib/ui/common/about_list.dart
@@ -3,14 +3,14 @@ import 'about_page.dart';
 final pages = [
   PageViewModel(
       'Starting',
-      'My programming world was created on 2015 since I went to college! I said \'Hello, World\' with C++, and learnt basics of programming with it. After that I started to learn Java. I practiced more and learnt Java Core. And then I tested a bit C# as a Windows Form, too :)',
+      'My programming world was created on 2015 since I went to college! I said \'Hello, World\' with C++, and learnt basics of programming with it. After that I started to learn Java. I practiced more and learnt Java Core. Then I tested a bit C# as a Windows Form, too :) \nAfter one year, I found my way around Android Developing! I started android development with java and after that I switched to Kotlin, my lovely language!',
       false),
   PageViewModel(
-      'Middling',
-      'After one year, I found my way around Android Developing! I started android development with java and that I switched to Kotlin, my lovely language!',
+      'Create a Team',
+      'On 2017, with 4 of my friends, made a small team that named ExceptionalDev Team and started working together with a lot of Motivation! After a year, we make the team bigger. We developed some applications in different fields, and we were working on IoT for about 6 month. I was there more for than 3 years',
       false),
   PageViewModel(
       'And Now...',
-      'On 2017, with 4 of my friends, made a small team that named ExceptionalDev Team and started working together with a lot of Motivation! After a year, we make the team bigger. I was there until 2020 and now I\'m looking for a new job!',
+      'After that I looked for a new job. I found Erfan Salamat Health Services Group and started working with them for a few month, but they got into trouble together and cancel the project and fired all developers!\nNow I\'m looking for a new job!',
       true),
 ];
