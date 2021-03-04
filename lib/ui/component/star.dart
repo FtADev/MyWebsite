9eeb1a3b497394db9100d4a20a0114d7ee3f7e6c
ftadev <akhlaghi.fatemeh@gmail.com>
2021-03-04 22:40:05 +0330

    Moving stars ***

--- lib/ui/component/star.dart
+++ lib/ui/component/star.dart
@@ -5,28 +5,64 @@ class Star extends StatelessWidget {
   final top;
   final right;
 
-  const Star({Key key, this.top, this.right,}) : super(key: key);
+  const Star({
+    Key key,
+    this.top,
+    this.right,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: top,
-      right: right,
-      child: CustomAnimation<double>(
+    final tween = MultiTween<DefaultAnimationProperties>()
+      ..add(
+        DefaultAnimationProperties.y,
+        Tween(begin: 0.0, end: 10.0),
+        Duration(seconds: 8),
+      )
+      ..add(
+        DefaultAnimationProperties.y,
+        Tween(begin: 10.0, end: 0.0),
+        Duration(seconds: 6),
+      )
+      ..add(
+        DefaultAnimationProperties.x,
+        Tween(begin: 0.0, end: 10.0),
+        Duration(seconds: 7),
+      )
+      ..add(
+        DefaultAnimationProperties.x,
+        Tween(begin: 10.0, end: 0.0),
+        Duration(seconds: 5),
+      );
+
+    return MirrorAnimation(
+      tween: tween,
+      duration: tween.duration,
+      curve: Curves.easeInOutSine,
+      builder: (context, child, MultiTweenValues positionValue) =>
+          CustomAnimation<double>(
         control: CustomAnimationControl.MIRROR,
         tween: Tween(begin: 1.5, end: 3.0),
         duration: Duration(milliseconds: 1000),
         curve: Curves.fastOutSlowIn,
         animationStatusListener: (status) {},
         builder: (context, child, value) {
-          return Container(
-                  width: value,
-                  height: value,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                );
+          return Positioned(
+            top: top % 2 == 0
+                ? top + positionValue.get(DefaultAnimationProperties.y)
+                : top,
+            right: right % 2 == 0
+                ? right + positionValue.get(DefaultAnimationProperties.x)
+                : right,
+            child: Container(
+              width: value,
+              height: value,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+            ),
+          );
         },
       ),
     );
