6b3980b5dfc115b7714947a1d92dca68b41e1363
ftadev <akhlaghi.fatemeh@gmail.com>
2021-05-16 22:22:19 +0430

    Minor

--- lib/ui/mobile/mobile_const.dart
+++ lib/ui/mobile/mobile_const.dart
@@ -1,9 +1,8 @@
 import 'package:MyWebsite/ui/common/screen.dart';
 
-class MobileConst extends Screen{
+class MobileConst extends Screen {
   final marginTop = 26.0;
- final marginLeft = 16.0;
-  final sizedBox = 10.0;
+  final marginLeft = 16.0;
   final bioFont1 = 20.0;
   final bioFont2 = 40.0;
   final bioFont3 = 18.0;
--- lib/ui/web/web_const.dart
+++ lib/ui/web/web_const.dart
@@ -3,7 +3,6 @@ import 'package:MyWebsite/ui/common/screen.dart';
 class WebConst extends Screen {
   final marginTop = 50.0;
   final marginLeft = 50.0;
-  final sizedBox = 20.0;
   final bioFont1 = 50.0;
   final bioFont2 = 70.0;
   final bioFont3 = 30.0;
