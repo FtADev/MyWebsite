ba9169c4b9a27e17da02f4a50a0d8e374a584d30
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-03-02 20:24:42 +0330

    + 404 page

---
+++ lib/ui/404_unknown_page.dart
@@ -0,0 +1,39 @@
+import 'package:MyWebsite/ui/background/fancy_background.dart';
+import 'package:flutter/material.dart';
+
+class UnknownPage extends StatelessWidget {
+  const UnknownPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        FancyBackgroundApp(),
+        Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Error 404",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontFamily: 'dekko',
+                ),
+              ),
+              Text(
+                "This Page Not Found!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'dekko',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
