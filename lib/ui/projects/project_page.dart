3d481c70c04fe2335155d96904c4143210329b68
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-03-12 13:43:36 +0330

    Change style of projects page

--- lib/ui/projects/project_page.dart
+++ lib/ui/projects/project_page.dart
@@ -1,7 +1,7 @@
+import 'package:MyWebsite/ui/background/fancy_background.dart';
 import 'package:MyWebsite/ui/projects/project_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ProjectPage extends StatelessWidget {
   static const String baseRoute = '/projects';
@@ -14,59 +14,71 @@ class ProjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    final double screenSize = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      project.title,
-                      style: TextStyle(
-                        fontFamily: 'dekko',
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
+      body: Stack(children: [
+        FancyBackgroundApp(),
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        project.title,
+                        style: TextStyle(
+                          fontFamily: 'dekko',
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    Text(
-                      project.otherDetail!,
-                      style: TextStyle(
-                        fontFamily: 'dekko',
-                        fontSize: 20,
+                      Text(
+                        project.otherDetail!,
+                        style: TextStyle(
+                          fontFamily: 'dekko',
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  ]),
-            ),
-            Container(
-              height: width > 1000
-                  ? MediaQuery.of(context).size.height * 0.8
-                  : MediaQuery.of(context).size.height * 0.5,
-              child: Swiper(
-                fade: 0.1,
-                itemCount: project.imageList!.length,
-                loop: true,
-                viewportFraction: project.isHorizontal!
-                    ? 0.6
-                    : width > 600
-                        ? 0.2
-                        : 0.5,
-                scale: 0.5,
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.asset(
-                    project.imageList![index],
-                    fit: BoxFit.fitWidth,
-                  );
-                },
+                    ]),
               ),
-            ),
-          ],
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: StaggeredGridView.count(
+                    crossAxisCount: screenSize < 600
+                        ? 4
+                        : screenSize < 1000
+                            ? 6
+                            : screenSize < 1600
+                                ? 8
+                                : 10,
+                    padding: const EdgeInsets.all(10),
+                    children: project.imageList!
+                        .map<Widget>(
+                          (item) => Image.asset(
+                            item,
+                            fit: BoxFit.fitHeight,
+                          ),
+                        )
+                        .toList(),
+                    staggeredTiles: project.imageList!
+                        .map<StaggeredTile>((_) => StaggeredTile.fit(2))
+                        .toList(),
+                    mainAxisSpacing: 50,
+                    crossAxisSpacing: 50,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
